e62a7a38417f6e0122eb7d8b14c3ef5e5737835b
Fabien Taillon <ftaillon@gmail.com>
2021-11-25 17:18:23 +0000

    Basic changes

--- setupScratchOrg.sh
+++ setupScratchOrg.sh
@@ -5,10 +5,10 @@ SCRATCH_ALIAS="${SCRATCH_ALIAS:-$PROJECT_NAME-`git branch | grep \* | cut -d ' '
 sfdx force:org:create --definitionfile config/project-scratch-def.json --setalias $SCRATCH_ALIAS --setdefaultusername
 echo '##### PUSHING METADATA #####'
 sfdx force:source:push --targetusername $SCRATCH_ALIAS
-#echo '##### ASSIGNING PERMISSIONS #####'
-#sfdx force:user:permset:assign --permsetname Timesheet --targetusername $SCRATCH_ALIAS
-#echo '##### IMPORTING DUMMY DATA #####'
-#sfdx texei:data:import --inputdir ./data --targetusername $SCRATCH_ALIAS
+echo '##### ASSIGNING PERMISSIONS #####'
+sfdx force:user:permset:assign --permsetname MyPermissionSet --targetusername $SCRATCH_ALIAS
+echo '##### IMPORTING DUMMY DATA #####'
+sfdx texei:data:import --inputdir ./data --targetusername $SCRATCH_ALIAS
 echo '##### CLEANING STANDARD LAYOUTS #####'
 sfdx texei:source:layouts:cleanorg --targetusername $SCRATCH_ALIAS
 echo '##### OPENING SCRATCH ORG #####'
